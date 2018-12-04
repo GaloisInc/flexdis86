e4d60a71c858c92b77e445d0a5ea52411cd285f4
Nathan Collins <conathan@galois.com>
2018-12-04 14:46:08 -0800

    Improve and document :/utils/dump.sh
    In particular, it now reads input in Intel syntax (it was already
    printing output in Intel syntax, but reading input in AT&T syntax ?!).
    
    Also, add a README explaining some of the the files in :/utils, and
    delete :/utils/dump_instr.sh, since its functionality was as subset of
    :/utils/dump.sh's functionality.

---
+++ utils/README.md
@@ -0,0 +1,7 @@
+- dump.sh: convert an Intel syntax ASM instruction into the flexdis86
+  II instruction representation.
+
+- dump_bytes.sh: disassemble a hex-encoded byte sequence ???
+
+- DumpInstr.hs: dissassemble a hex-encoded byte sequence into the
+  flexdis86 II instruction representation.
--- utils/dump.sh
+++ utils/dump.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
+# Assemble the ASM instruction given by the first argument (Intel
+# syntax) and then dump the flexdix86 representation of that
+# instruction to stdout. For example
+#
+#     ./dump.sh "add %eax, %eax"
+#
+# This script assumes that the flexdis86 'DumpInstr' binary is on
+# PATH. So, you probably want to do
+#
+#     cabal new-exec ./dump.sh <asm>
+#
+# or
+#
+#     stack exec ./dump.sh <asm>
+#
+# to make 'DumpInstr' available.
+
 set -e
 
-out=`mktemp testfile-XXXXXX`
+if ! which DumpInstr &>/dev/null; then
+  echo "No DumpInstr binary on PATH. See below:" >&2
+  echo "" >&2
+  cat $0 >&2
+  exit 1
+fi
+
+mkdir -p tmp
+out=`mktemp tmp/testfile-XXXXXX.S`
 out_obj=${out%.S}.o
 
 cat <<EOF > $out
+    .intel_syntax noprefix
     .text
     $@
 EOF
 
-gcc -x assembler -c $out
+gcc -x assembler -c $out -o $out_obj
 objdump --disassembler-options=intel-mnemonic -dw $out_obj | sed -n 's/^[[:space:]]*0:[[:space:]]*//p'
-objdump --disassembler-options=intel-mnemonic -dw $out_obj | sed -n 's/^[[:space:]]*0:[[:space:]]*//p' | cut -f1 | xargs ../.cabal-sandbox/bin/DumpInstr
-rm -f $out $out_obj
+objdump --disassembler-options=intel-mnemonic -dw $out_obj | sed -n 's/^[[:space:]]*0:[[:space:]]*//p' | cut -f1 | xargs DumpInstr
+echo $out $out_obj
