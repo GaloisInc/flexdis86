20123ca252a1093657a2cd83540e532d0afb6f35
Langston Barrett <langston.barrett@gmail.com>
2026-05-12 14:38:44 +0000

    Move prefix byte constants to Flexdis86.Prefixes.Bytes
    Lets lower-level modules that need the raw byte constants
    (Flexdis86.Prefixes.Seen, Flexdis86.Prefixes.Required) import them
    without pulling in the whole Flexdis86.Prefixes module. Flexdis86.Prefixes
    continues to re-export them so existing callers are unaffected.

--- src/Flexdis86/Prefixes.hs
+++ src/Flexdis86/Prefixes.hs
@@ -22,81 +22,24 @@ module Flexdis86.Prefixes
   , prNoTrack
   , prAddrSize
   , no_seg_prefix
-  , notrackPrefixByte
-  , addrSizeOverrideByte
-  , operandSizeOverrideByte
-  , lockPrefixByte
-  , repNZPrefixByte
-  , repPrefixByte
-  , esPrefixByte
-  , csPrefixByte
-  , ssPrefixByte
-  , dsPrefixByte
-  , fsPrefixByte
-  , gsPrefixByte
-  , isRexPrefixByte
+  , module Flexdis86.Prefixes.Bytes
   , setDefault
   , LockPrefix(..)
   , ppLockPrefix
   ) where
 
 import qualified Control.DeepSeq as DS
-import qualified Data.Bits as B
 import           Data.Word ( Word8 )
 import           GHC.Generics
 import           Lens.Micro (Lens', lens, (^.))
 import           Numeric ( showHex )
 import qualified Prettyprinter as PP
 
+import           Flexdis86.Prefixes.Bytes
 import           Flexdis86.Prefixes.REX (REX)
 import qualified Flexdis86.VEX.Seen as VEX
 import           Flexdis86.Segment
 import           Flexdis86.Sizes
-
--- | The byte used for the @notrack@ CET prefix.
---
--- This should not be confused with the DS segment override, which uses the same
--- byte. Instructions uphold the convention that notrack and segment overrides
--- are not used simultaneously to avoid ambiguity.
-notrackPrefixByte :: Word8
-notrackPrefixByte = 0x3e
-
--- | The address-size override prefix byte.
-addrSizeOverrideByte :: Word8
-addrSizeOverrideByte = 0x67
-
--- | The operand-size override prefix byte.
-operandSizeOverrideByte :: Word8
-operandSizeOverrideByte = 0x66
-
--- | The @LOCK@ prefix byte.
-lockPrefixByte :: Word8
-lockPrefixByte = 0xF0
-
--- | The @REPNZ@/@REPNE@ prefix byte.
-repNZPrefixByte :: Word8
-repNZPrefixByte = 0xF2
-
--- | The @REP@/@REPZ@/@REPE@ prefix byte.
-repPrefixByte :: Word8
-repPrefixByte = 0xF3
-
--- | The segment-override prefix bytes: ES, CS, SS, DS, FS, GS.
---
--- Note that the DS byte (0x3E) is the same byte as 'notrackPrefixByte'; which
--- role it plays depends on the instruction.
-esPrefixByte, csPrefixByte, ssPrefixByte, dsPrefixByte :: Word8
-fsPrefixByte, gsPrefixByte :: Word8
-esPrefixByte = 0x26
-csPrefixByte = 0x2E
-ssPrefixByte = 0x36
-dsPrefixByte = 0x3E
-fsPrefixByte = 0x64
-gsPrefixByte = 0x65
-
--- | Is the given byte a REX prefix (0x40..0x4F)?
-isRexPrefixByte :: Word8 -> Bool
-isRexPrefixByte b = b B..&. 0xF0 == 0x40
 
 ------------------------------------------------------------------------
 -- SegmentPrefix
